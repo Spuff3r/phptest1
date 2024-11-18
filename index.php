a2885eb65bdb28464c92558605d197fb09d1fd08
Spuff3r <long.ho8541@gmail.com>
2024-11-18 11:24:14 +0100

    start

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <input type="text" name="fornamn" placeholder="Förnamn">
+        <input type="text" name="efternamn" placeholder="Efternamn">
+        <input type="text" name="submit" placeholder="Skicka">
+    </form>
+
+</body>
+</html>
